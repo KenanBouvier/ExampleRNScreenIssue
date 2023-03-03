29d1f581ad4af3e7b6d823bee1b144e739fbf8e7
Kenan Bouvier <kenan.bouvier@chevinfleet.com>
2023-03-03 14:22:11 +0000

    Refactor

--- App.tsx
+++ App.tsx
@@ -1,61 +1,37 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import {
   View,
   Text,
-  FlatList,
   RefreshControl,
   SectionList,
-  SafeAreaView,
   Button,
   ScrollView,
 } from 'react-native';
-import {
-  NavigationContainer,
-  useFocusEffect,
-  useNavigation,
-} from '@react-navigation/native';
-// import RNBootSplash from 'react-native-bootsplash';
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 
 import {useState} from 'react';
-import {DATA, sectionData} from './data';
+import {sectionData} from './data';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useHeaderHeight} from '@react-navigation/elements';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 function HomeScreen() {
   const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
-  const [render, setRender] = useState<boolean>(false);
   const headerHeight = useHeaderHeight();
-
-  // React.useEffect(() => {
-  //   setTimeout(() => {
-  //     setRender(true);
-  //   }, 1000);
-  // }, []);
   const navigation = useNavigation();
-
-  React.useEffect(() => {
-    // @ts-ignore
-    const unsubscribe = navigation.addListener('transitionEnd', e => {
-      setRender(true);
-    });
-
-    return unsubscribe;
-  }, [navigation]);
-
-  // useFocusEffect(
-  //   React.useCallback(() => {
-  //     setTimeout(() => {
-  //       setRender(true);
-  //     });
-  //   }, []),
-  // );
-
   const insets = useSafeAreaInsets();
 
-  console.log('headerHeight', headerHeight);
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLargeTitleShadowVisible: true,
+      headerShown: true,
+      headerTransparent: true,
+      headerSearchBarOptions: {
+        placeholder: 'Search',
+        cancelButtonText: 'Cancel',
+      },
+    });
+  }, [navigation]);
 
   return (
     <View
@@ -65,99 +41,38 @@ function HomeScreen() {
         marginBottom: insets.bottom,
       }}>
       <>
-        {/* <ScrollView
-        contentInsetAdjustmentBehavior="always"
-        refreshControl={
-          <RefreshControl
-            refreshing={false}
-            tintColor="red"
-            progressViewOffset={40}
-            onRefresh={async () => {
-              // setIsRefreshing(true);
-              // // await new Promise(res => {
-              // //   setTimeout(() => res(undefined), 10000);
-              // // });
-              // setIsRefreshing(false);
-            }}
-          />
-        }>
-        <Text />
-      </ScrollView> */}
-        {/* <FlatList
-        data={DATA}
-        renderItem={({item}) => <Text>{item.title}</Text>}
-        // contentInset={{
-        //   top: headerHeight,
-        // }}
-        contentInsetAdjustmentBehavior="always"
-        refreshControl={
-          <RefreshControl
-            refreshing={false}
-            tintColor="red"
-            progressViewOffset={40}
-            onRefresh={async () => {
-              // setIsRefreshing(true);
-              // // await new Promise(res => {
-              // //   setTimeout(() => res(undefined), 10000);
-              // // });
-              // setIsRefreshing(false);
-            }}
-          />
-        }
-      /> */}
-
-        {render && (
-          <SectionList
-            sections={sectionData}
-            contentInsetAdjustmentBehavior="automatic"
-            keyExtractor={(item, index) => item + index}
-            initialNumToRender={60}
-            maxToRenderPerBatch={60}
-            refreshControl={
-              <RefreshControl
-                tintColor={'black'}
-                progressViewOffset={40}
-                refreshing={isRefreshing}
-                onRefresh={async () => {
-                  setIsRefreshing(true);
-                  await new Promise(res => {
-                    setTimeout(() => res(undefined), 1000);
-                  });
-                  setIsRefreshing(false);
-                }}
-                size={100}
-              />
-            }
-            renderItem={({item}) => (
-              <View>
-                <Text>{item}</Text>
-              </View>
-            )}
-            renderSectionHeader={({section: {title}}) => <Text>{title}</Text>}
-          />
-        )}
-
-        {/* <FlatList
-        data={DATA}
-        renderItem={({item}) => <Text>{item.title}</Text>}
-        // contentInset={{
-        //   top: headerHeight,
-        // }}
-        contentInsetAdjustmentBehavior="always"
-        refreshControl={
-          <RefreshControl
-            refreshing={isRefreshing}
-            tintColor="red"
-            onRefresh={async () => {
-              // setIsRefreshing(true);
-              // await new Promise(res => {
-              //   setTimeout(() => res(undefined), 10000);
-              // });
-              // setIsRefreshing(false);
-            }}
-          />
-        }
-      /> */}
+        <SectionList
+          sections={sectionData}
+          contentInsetAdjustmentBehavior="automatic"
+          keyExtractor={(item, index) => item + index}
+          initialNumToRender={60}
+          maxToRenderPerBatch={60}
+          refreshControl={
+            <RefreshControl
+              tintColor={'black'}
+              progressViewOffset={40}
+              refreshing={isRefreshing}
+              onRefresh={async () => {
+                setIsRefreshing(true);
+                await new Promise(res => {
+                  setTimeout(() => res(undefined), 1000);
+                });
+                setIsRefreshing(false);
+              }}
+              size={100}
+            />
+          }
+          renderItem={({item}) => (
+            <View>
+              <Text>{item}</Text>
+            </View>
+          )}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={{backgroundColor: '#d3d3d3', fontSize: 20}}>
+              {title}
+            </Text>
+          )}
+        />
       </>
     </View>
   );
@@ -184,13 +99,7 @@ function HomeScreen1() {
   );
 }
 
-// const Stack = createStackNavigator();
 function App() {
-  // React.useEffect(() => {
-  //   RNBootSplash.hide({ fade: true, duration: 200 }).catch(e => {
-  //     console.error(`Splash error: ${(e as Error).message}`);
-  //   });
-  // }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator
---
+++ Notes.md
@@ -0,0 +1,31 @@
+# Steps
+
+- Add search Bar
+- Replicate the search bar UI bug
+
+- Clean up example app (Refactor) + Easy to read
+- Rename screens to more descriptive: e.g. Popular dishes, Home,
+
+- Verify the bug still exists in release mode.
+
+## Github issue
+
+Title: (short sentence) e.g. glitchy iOS search bar when using header large titles
+
+Body:
+Intro(hi...)
+Struggled to get this particular scenario woriing: large titlees, pull to refresh ,section list with sticky headers and ios search bar using the native screens.
+
+Here is a video demonstration the glitchy UI behavior: <show video>
+
+Can you let me know if i'm doing anything wrong or if you had any advice?
+
+## steps to reporoduce
+
+git clone <>
+cd <>
+npm i
+cd ios
+pod install
+npm start
+...
